4b332823231a8027a8cc9498aab143bade352b40
Mahipat <mj@mojaave.com>
2020-05-09 11:11:35 +0530

    Fetch channel statistics from the youtube

--- plugins/gatsby-source-videos-youtube/gatsby-node.js
+++ plugins/gatsby-source-videos-youtube/gatsby-node.js
@@ -25,8 +25,12 @@ const createNodeFromData = (item, nodeType, helpers) => {
   return node;
 };
 
+// https://www.googleapis.com/youtube/v3/channels?part=statistics%2Csnippet&id=UCpi2S8wW4xLlUCVryhyBtsA&key=[YOUR_API_KEY] HTTP/1.1
+
+
 const getAllData = async ({ type = 'playlists', query = '', API_KEY }) => {
-  const URL = `https://www.googleapis.com/youtube/v3/${type}?part=snippet,contentDetails&maxResults=50&key=${API_KEY}${query}`;
+  const URL = `https://www.googleapis.com/youtube/v3/${type}?part=statistics,snippet,contentDetails&maxResults=50&key=${API_KEY}${query}`;
+  console.log('URL', URL)
   let responseData = [];
   const response = await fetch(URL).then(res => res.json());
   if (response.nextPageToken) {
@@ -57,6 +61,10 @@ exports.onPreInit = () => console.log('Loaded source-plugin');
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions;
   const typeDefs = `
+  type Channel implements Node {
+    id: ID!
+    local: File @link
+  }
   type Playlist implements Node {
     id: ID!
     local: File @link
@@ -101,6 +109,7 @@ exports.sourceNodes = async function sourceNodes(
   // console.log(await cache.get(`hello`))
 
   // touch nodes to ensure they aren't garbage collected
+  getNodesByType('Channel').forEach(node => touchNode({ nodeId: node.id }));
   getNodesByType('Playlist').forEach(node => touchNode({ nodeId: node.id }));
   getNodesByType('Video').forEach(node => touchNode({ nodeId: node.id }));
   // getNodesByType(AUTHOR_NODE_TYPE).forEach(node =>
@@ -111,7 +120,17 @@ exports.sourceNodes = async function sourceNodes(
   // const cacheKey = "your-source-data-key"
   // let sourceData = await cache.get(cacheKey)
   let playlistData = await cache.get(`playlists`);
+  let channelsData = await cache.get(`channels`);
   let allVideos = await cache.get(`videos`);
+
+  if (!channelsData) {
+    channelsData = await getAllData({
+      type: 'channels',
+      query: `&id=${channelID}`,
+      API_KEY
+    });
+    await cache.set(`channels`, channelsData);
+  }
 
   if (!playlistData) {
     playlistData = await getAllData({
@@ -135,6 +154,11 @@ exports.sourceNodes = async function sourceNodes(
     allVideos = await Promise.all(videoRequest);
     await cache.set(`videos`, allVideos);
   }
+
+  channelsData.forEach(channel => {
+    channel.channelId = channel.id;
+    createNodeFromData(channel, 'Channel', helpers);
+  });
 
   playlistData.forEach((playlist, index) => {
     const videoNodeId = [];
@@ -167,10 +191,11 @@ exports.onCreateNode = async ({
   node
 }) => {
   if (
-    (node.internal.type === 'Playlist' || node.internal.type === 'Video') &&
+    (node.internal.type === 'Channel' || node.internal.type === 'Playlist' || node.internal.type === 'Video') &&
     node.snippet.thumbnails &&
     node.snippet.thumbnails.high
   ) {
+    console.log("Channel", node.internal.type)
     const imageName = node.snippet.thumbnails.high.url.match(/([^/]*)\/*$/)[1];
     const imageCacheKey = `local-image-${imageName}-${node.id}`;
     const cachedImage = await cache.get(imageCacheKey);
--- themes/gatsby-theme-factly/src/pages/playlists.js
+++ themes/gatsby-theme-factly/src/pages/playlists.js
@@ -8,26 +8,17 @@ import { Play } from '../components/icons';
 import Footer from '../components/footer';
 import logo from '../static/images/factly-video.jpg';
 
-const items = Array(20).fill({
-  title:
-    'A video clip from a web series is being falsely shared as ‘Sadhu strangling a policeman brutally’',
-  excerpt:
-    'After POTUS Donald Trump announced that USA is halting its funding to the WHO, there has been a lot of debate around WHO’s finances. So, who funds the WHO? What is the size of its budget? Here is a detailed explainer.',
-  author: ['Pavithra K M', 'Bharath Kancharla'],
-  time: '2 Min',
-  image: ''
-});
-
 function Playlists({ data }) {
+  const { allPlaylist: { nodes: playlists }, channel } = data;
   console.log(data);
-  const [postItems, setPostItems] = useState(items.slice(0, 2));
+  const [postItems, setPostItems] = useState(playlists.slice(0, 2));
   const [hasNextPage, setHasNextPage] = useState(true);
 
   const handleLoadMore = () => {
     if (!hasNextPage) return false;
-    const nextPageItems = items.slice(postItems.length, postItems.length + 2);
+    const nextPageItems = playlists.slice(postItems.length, postItems.length + 2);
     setPostItems([...postItems, ...nextPageItems]);
-    setHasNextPage(postItems.length < items.length);
+    setHasNextPage(postItems.length < playlists.length);
   };
 
   return (
@@ -39,25 +30,29 @@ function Playlists({ data }) {
             src="https://source.unsplash.com/collection/9419734/1000x200"
             className="w-full -mt-16"
           />
-          <div className="flex justify-between items-center border-b p-6">
+          <div className="flex justify-between items-start border-b p-6">
             <div className="flex flex-row">
-              <img alt="" src={logo} className="h-24" />
-              <div className="px-4">
-                <h5 className="heading">Factly Youtube</h5>
-                <span className="text-gray-600 text-xs md:text-sm pt-2">
-                  30 k Subscribers
+              {/* <Img alt={channel.snippet.title} fluid={channel.local.childImageSharp.fluid} className="h-24" /> */}
+              <img alt={channel.snippet.title} src={channel.snippet.thumbnails.high.url} className="h-24" />
+              <div className="flex flex-col justify-start px-4">
+                <h5 className="heading">{channel.snippet.title}</h5>
+                <span className="text-gray-600 text-xs md:text-sm pb-2">
+                  {Number(channel.statistics.subscriberCount).toLocaleString()} Subscribers
                 </span>
+                <p className="text-base hidden md:inline">{channel.snippet.description}</p>
               </div>
             </div>
-            <button
+            <a
+              href="https://www.youtube.com/channel/UCpi2S8wW4xLlUCVryhyBtsA"
+              target="_blank"
               type="button"
               className="block lg:px-4 uppercase font-medium text-sm focus:outline-none bg-gray-300 rounded p-2"
             >
               Subscribe
-            </button>
+            </a>
           </div>
           <div className="flex flex-row flex-wrap p-6 justify-center sm:justify-start items-center sm:items-start">
-            {data.allPlaylist.nodes.map(playlist => (
+            {playlists.map(playlist => (
               <Link
                 key={playlist.id}
                 className="flex flex-col w-full sm:w-1/3 lg:w-1/4 xl:w-1/5 no-underline hover:no-underline sm:pr-6 pb-4 mb-6"
@@ -115,6 +110,29 @@ export default Playlists;
 
 export const query = graphql`
   query PlaylistsPageQuery {
+    channel {
+      statistics {
+        subscriberCount
+        videoCount
+      }
+      local {
+        childImageSharp {
+          fluid(maxWidth: 300, quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      snippet {
+        thumbnails{
+          high{
+            url
+          }
+        }
+        customUrl
+        description
+        title
+      }
+    }
     allPlaylist {
       totalCount
       nodes {
