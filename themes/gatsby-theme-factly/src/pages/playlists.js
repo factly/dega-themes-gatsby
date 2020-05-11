837a0166f2125c18ddda797b1bb28f825b9c1e8f
Mahipat <mj@mojaave.com>
2020-05-11 19:01:54 +0530

    Updated playlist page design

--- themes/gatsby-theme-factly/src/pages/playlists.js
+++ themes/gatsby-theme-factly/src/pages/playlists.js
@@ -31,36 +31,31 @@ function Playlists({ data }) {
   return (
     <Layout>
       <div className="flex flex-col lg:flex-row justify-between lg:border-b">
-        <div className="main-content w-full -my-8 lg:my-0">
-          <img
-            alt=""
-            src="https://source.unsplash.com/collection/9419734/1000x200"
-            className="w-full -mt-16"
-          />
-          <div className="flex justify-between items-start border-b p-6">
-            <div className="flex flex-row">
-              {/* <Img alt={channel.snippet.title} fluid={channel.local.childImageSharp.fluid} className="h-24" /> */}
-              <img alt={channel.snippet.title} src={channel.snippet.thumbnails.high.url} className="h-24" />
-              <div className="flex flex-col justify-start px-4">
-                <h5 className="heading">{channel.snippet.title}</h5>
-                <span className="text-gray-600 text-xs md:text-sm pb-2">
-                  {Number(channel.statistics.subscriberCount).toLocaleString()} Subscribers
-                </span>
-                <p className="text-base hidden md:inline">{channel.snippet.description}</p>
-              </div>
-            </div>
-            <a
-              rel="noopener noreferrer"
-              href="https://www.youtube.com/channel/UCpi2S8wW4xLlUCVryhyBtsA"
-              target="_blank"
-              type="button"
-              className="block lg:px-4 uppercase font-medium text-sm focus:outline-none bg-gray-300 rounded p-2"
-            >
-              Subscribe
-            </a>
-          </div>
+        <div className="main-content w-full -my-8">
           <div>
-            <ul className="flex px-8 pt-8 lg:pt-32 bg-gray-300">
+          <div className="flex justify-between items-start border-b p-6 bg-gray-300">
+              <div className="flex flex-row">
+                {/* <Img alt={channel.snippet.title} fluid={channel.local.childImageSharp.fluid} className="h-24" /> */}
+                <img alt={channel.snippet.title} src={channel.snippet.thumbnails.high.url} className="h-24" />
+                <div className="flex flex-col justify-start px-4">
+                  <h5 className="heading">{channel.snippet.title}</h5>
+                  <span className="text-gray-600 text-xs md:text-sm pb-2">
+                    {Number(channel.statistics.subscriberCount).toLocaleString()} Subscribers
+                  </span>
+                  <p className="text-base hidden md:inline">{channel.snippet.description}</p>
+                </div>
+              </div>
+              <a
+                rel="noopener noreferrer"
+                href="https://www.youtube.com/channel/UCpi2S8wW4xLlUCVryhyBtsA"
+                target="_blank"
+                type="button"
+                className="block lg:px-4 uppercase font-medium text-sm focus:outline-none bg-gray-100 rounded p-2"
+              >
+                Subscribe
+              </a>
+            </div>
+            <ul className="flex px-8 pt-8 bg-gray-300">
               {tabs.map(tab => (
                 <li className="-mb-px mr-1">
                   <button
