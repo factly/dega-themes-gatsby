e6198a04e1b5f1bf31048f3231dfd546fb59fcb0
Mourya Prasanth <mouryaprasanthp@gmail.com>
2020-09-29 18:12:45 +0530

    add fonts to tailwind config

--- themes/gatsby-theme-factly/tailwind.config.js
+++ themes/gatsby-theme-factly/tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        inter: [
+          'Inter, Roboto, "Helvetica Neue", Arial, "Noto Sans", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+        ],
+        metropolis: 'Metropolis, Georgia, Cambria, "Times New Roman", Times, serif',
+      },
       inset: {
         '-16': '-4rem',
         '-20': '-5rem',
