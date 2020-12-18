f885147e5ec7fc4d8c6fe59ed2a2ed167487086d
Mourya Prasanth <mouryaprasanthp@gmail.com>
2020-12-18 17:39:09 +0530

    add social share to amp pages

--- themes/gatsby-theme-factly/src/components/Layout.amp.js
+++ themes/gatsby-theme-factly/src/components/Layout.amp.js
@@ -63,7 +63,27 @@ const LayoutAmp = (props) => (
               </a>
             </div>
           </nav>
-          <div style={{ maxWidth: '702px', margin: '0 auto' }}>{children}</div>
+          <div style={{ maxWidth: '702px', margin: '0 auto' }}>
+            {children}
+            <div style={{ margin: '1rem 0', textAlign: 'center' }}>
+              <amp-social-share type="twitter" aria-label="Share on Twitter"></amp-social-share>{' '}
+              <amp-social-share type="facebook" aria-label="Share on Facebook"></amp-social-share>{' '}
+              <amp-social-share type="system" aria-label="Share"></amp-social-share>{' '}
+              <amp-social-share type="email" aria-label="Share"></amp-social-share>{' '}
+              <amp-social-share
+                type="linkedin"
+                width="60"
+                height="44"
+                data-param-text="Hello "
+                data-param-url="https://factly.in"
+                aria-label="Share on LinkedIn"
+              ></amp-social-share>{' '}
+              <amp-social-share
+                type="whatsapp"
+                data-param-text="Check out this article: TITLE - CANONICAL_URL"
+              ></amp-social-share>
+            </div>
+          </div>
         </>
       );
     }}
