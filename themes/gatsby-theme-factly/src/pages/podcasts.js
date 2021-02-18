d10ad41f2b2ecdf0969a55c05811d5c4aa0ebcb9
Mourya Prasanth <mouryaprasanthp@gmail.com>
2021-02-18 15:48:04 +0530

    fix podcasts script issue & add meta tags for SEO

--- themes/gatsby-theme-factly/src/pages/podcasts.js
+++ themes/gatsby-theme-factly/src/pages/podcasts.js
@@ -22,11 +22,52 @@ const PodcastsPage = () => {
   return (
     <Layout>
       <Helmet>
-        <script
-          type="text/javascript"
-          charset="utf-8"
-          src="https://www.buzzsprout.com/1663999.js?container_id=buzzsprout-large-player-1663999&player=large"
-        ></script>
+        <meta name="title" content="Un-Viral" />
+        <meta name="author" content="Factly" />
+        <meta
+          name="description"
+          content="Un-Viral is a podcast from Factly where we tackle that dangerous combination of the
+              two kinds of virality—misinformation about health. Tune in to discern the real from
+              the fake, and remember to Un-Viral."
+        />
+
+        <meta httpEquiv="Content-Type" content="text/html;charset=utf-8" />
+        <meta name="keywords" content="Factly, Podcast, Unviral" />
+
+        <meta name="apple-itunes-app" content="app-id=1552026867" />
+        {/* <link type="application/rss+xml" rel="alternate" title="Un-Viral" href="https://feeds.buzzsprout.com/1663999.rss"/> */}
+        <link rel="apple-touch-icon" href={unviralImg} />
+        <link rel="canonical" href="https://health.factly.in/podcasts" />
+        <link rel="image_src" href={unviralImg} />
+
+        <title>Un-Viral</title>
+        <meta
+          name="description"
+          content="Un-Viral is a podcast from Factly where we tackle that dangerous combination of the two kinds of virality—misinformation about health. Tune in to discern the real from the fake, and remember to Un-Viral."
+        />
+        <meta property="og:site_name" content="Factly Health" />
+        <meta property="og:url" content="https://health.factly.in/podcasts" />
+        <meta property="og:title" content="Un-Viral" />
+        <meta
+          property="og:description"
+          content="Un-Viral is a podcast from Factly where we tackle that dangerous combination of the two kinds of virality—misinformation about health. Tune in to discern the real from the fake, and remember to Un-Viral."
+        />
+        <meta property="og:type" content="website" />
+
+        <meta property="og:image" content={unviralImg} />
+        <meta property="og:image:width" content="250" />
+        <meta property="og:image:height" content="250" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@factly" />
+        <meta name="twitter:title" content="Un-Viral" />
+        <meta
+          name="twitter:description"
+          content="Un-Viral is a podcast from Factly where we tackle that dangerous combination of the two kinds of virality—misinformation about health. Tune in to discern the real from the fake, and remember to Un-Viral."
+        />
+        <meta name="twitter:image" content={unviralImg} />
+
+        {/* 
+  <link rel="shortcut icon" href="/favicon.ico"> */}
       </Helmet>
       <div sx={{ maxWidth: 1200, mx: 'auto', textAlign: 'center', px: 6 }}>
         <div sx={{ display: 'grid', gridTemplateColumns: ['1fr', null, '1fr 1fr'], pt: 8, mb: 8 }}>
