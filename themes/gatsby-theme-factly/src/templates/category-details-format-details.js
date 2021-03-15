6197aad8468eb20de31bd4e69a20269225e84a10
Mourya Prasanth <mouryaprasanthp@gmail.com>
2021-03-15 23:37:34 +0530

    add category description for categories page

--- themes/gatsby-theme-factly/src/templates/category-details-format-details.js
+++ themes/gatsby-theme-factly/src/templates/category-details-format-details.js
@@ -2,17 +2,49 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import { graphql } from 'gatsby';
 import { jsx } from 'theme-ui';
-import FormatPageLayout from '../components/FomatPageLayout';
+import parseEditorJsData from '../utils/parseEditorJsData';
+import FormatPageLayout from '../components/FormatPageLayout';
 
 function CategoryDetailsFormat({ data }) {
   const { dega } = data;
 
+  const [readMore, setReadMore] = React.useState(true);
+  const header = (item) => {
+    return (
+      <div sx={{ mb: 6 }}>
+        <h1 sx={{ textAlign: 'center', fontSize: [5, 6], mb: 4, textTransform: 'capitalize' }}>
+          {item.name} 
+        </h1>
+        <div
+          id="category-description"
+          sx={{
+            maxHeight: (theme) =>
+              readMore ? `calc( 1rem + ${theme.lineHeights.normal}em * 6 )` : '100%',
+            overflow: 'hidden',
+            px: 4,
+          }}
+        >
+          {parseEditorJsData(item.description)}
+        </div>
+        {item.description && (
+          <button
+            type="button"
+            onClick={() => setReadMore((prev) => !prev)}
+            sx={{ px: 4, color: (theme) => `${theme.colors.blue[5]}` }}
+          >
+            {readMore ? 'Read more' : 'Read less'}
+          </button>
+        )}
+      </div>
+    );
+  };
   return (
     <FormatPageLayout
       type="categories"
       posts={dega.posts.nodes}
       formats={dega.formats.nodes}
       item={dega.category}
+      header={header}
     />
   );
 }
