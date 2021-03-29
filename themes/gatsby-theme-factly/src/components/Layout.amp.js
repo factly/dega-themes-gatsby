1afb4bac8b20ba1afc3af7bd0c3dba20bb548728
Mourya Prasanth <mouryaprasanthp@gmail.com>
2021-03-29 20:21:44 +0530

    remove canonical from amp

--- themes/gatsby-theme-factly/src/components/Layout.amp.js
+++ themes/gatsby-theme-factly/src/components/Layout.amp.js
@@ -31,7 +31,6 @@ const LayoutAmp = (props) => (
         <>
           <Seo
             title={`${space.site_title} ${space.tag_line}`}
-            canonical={space.site_address}
             image={
               space.logo?.url?.proxy &&
               `${space.logo?.url?.proxy}?resize:fill:1200:330/enlarge:1/gravity:sm/pd:150:40:150:40`
