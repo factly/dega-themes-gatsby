a7dc2221965b78446983383c6a92d549b06c4ba8
Mourya Prasanth <mouryaprasanthp@gmail.com>
2021-04-27 15:55:55 +0530

    add workbox runtimecache overrides

--- themes/gatsby-theme-factly/gatsby-browser.js
+++ themes/gatsby-theme-factly/gatsby-browser.js
@@ -1,3 +1,5 @@
 import './src/static/css/normalize.css';
 import './src/static/fonts/fonts.css';
 import './src/static/css/tailwind.css';
+
+export const onServiceWorkerUpdateReady = () => window.location.reload();
--- themes/gatsby-theme-factly/gatsby-config.js
+++ themes/gatsby-theme-factly/gatsby-config.js
@@ -4,46 +4,6 @@ const editorjsHTML = require('editorjs-html');
 
 // const autoprefixer = require(`autoprefixer`);
 // const cssnano = require(`cssnano`);
-const workboxOptions = {
-  importWorkboxFrom: `local`,
-  // globDirectory: rootDir,
-  // globPatterns,
-  // modifyURLPrefix: {
-  //   // If `pathPrefix` is configured by user, we should replace
-  //   // the default prefix with `pathPrefix`.
-  //   '/': `${pathPrefix}/`,
-  // },
-  cacheId: `gatsby-plugin-offline`,
-  // Don't cache-bust JS or CSS files, and anything in the static directory,
-  // since these files have unique URLs and their contents will never change
-  dontCacheBustURLsMatching: /(\.js$|\.css$|static\/)/,
-  runtimeCaching: [
-    {
-      // Use cacheFirst since these don't need to be revalidated (same RegExp
-      // and same reason as above)
-      urlPattern: /(\.js$|\.css$|static\/)/,
-      handler: `NetworkFirst`,
-    },
-    {
-      // page-data.json files, static query results and app-data.json
-      // are not content hashed
-      urlPattern: /^https?:.*\/page-data\/.*\.json/,
-      handler: `NetworkFirst`,
-    },
-    {
-      // Add runtime caching of various other page resources
-      urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-      handler: `StaleWhileRevalidate`,
-    },
-    {
-      // Google Fonts CSS (doesn't end in .css so we need to specify it)
-      urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
-      handler: `StaleWhileRevalidate`,
-    },
-  ],
-  skipWaiting: true,
-  clientsClaim: true,
-};
 module.exports = ({
   client,
   api,
@@ -217,7 +177,33 @@ module.exports = ({
     {
       resolve: 'gatsby-plugin-offline',
       precachePages: ['/about/', '/podcasts/'],
-      workboxConfig: workboxOptions,
+      workboxConfig: {
+        runtimeCaching: [
+          {
+            // Use cacheFirst since these don't need to be revalidated (same RegExp
+            // and same reason as above)
+            urlPattern: /(\.js$|\.css$|static\/)/,
+            handler: `NetworkFirst`,
+          },
+          {
+            // page-data.json files, static query results and app-data.json
+            // are not content hashed
+            urlPattern: /^https?:.*\/page-data\/.*\.json/,
+            handler: `NetworkFirst`,
+          },
+          {
+            // Add runtime caching of various other page resources
+            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|avif|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: `NetworkFirst`,
+          },
+          {
+            // Google Fonts CSS (doesn't end in .css so we need to specify it)
+            urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+            handler: `StaleWhileRevalidate`,
+          },
+        ],
+      },
+      debug: true,
     },
   ].filter(Boolean),
 });
