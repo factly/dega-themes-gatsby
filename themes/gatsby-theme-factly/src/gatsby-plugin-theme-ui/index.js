9468fe54247e377e1e9a7bb1b3aa797c08f8fb4a
Mourya Prasanth <mouryaprasanthp@gmail.com>
2021-05-02 08:40:49 +0530

    closes #4

--- themes/gatsby-theme-factly/src/gatsby-plugin-theme-ui/index.js
+++ themes/gatsby-theme-factly/src/gatsby-plugin-theme-ui/index.js
@@ -2,7 +2,7 @@ export default {
   // useLocalStorage: false,
   // useCustomProperties: false,
   // useColorSchemeMediaQuery: false,
-  // useBodyStyles: false,
+  // useRootStyles: true,
   useBorderBox: true,
   breakpoints: ['640px', '768px', '1024px', '1280px', '1536px'],
   // sm, md, lg, xl
