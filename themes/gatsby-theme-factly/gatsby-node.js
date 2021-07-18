a5118db5e4927b7801eca6ae655d68a4d561f1a0
Mourya Prasanth <mouryaprasanthp@gmail.com>
2021-07-19 01:38:23 +0530

    update queries in gatsby-theme-factly's gatsby-node.js

--- themes/gatsby-theme-factly/gatsby-node.js
+++ themes/gatsby-theme-factly/gatsby-node.js
@@ -84,48 +84,41 @@ exports.onPreBootstrap = ({ store }) => {
 };
 
 exports.createPages = async ({ graphql, actions, store, reporter }, { spaceId, homepage = 1 }) => {
-  console.log({ spaceId });
   const { createPage } = actions;
-  // const total = await grapql(`
-  // `);
   const space = await graphql(`
     query SpaceQuery {
-      dega {
-        space {
-          id
-          name
-          slug
-          site_title
-          tag_line
-          description
-          site_address
-          logo {
-            url
-          }
-          logo_mobile {
-            url
-          }
-          fav_icon {
-            url
-          }
-          mobile_icon {
-            url
-          }
-          verification_codes
-          social_media_urls
-          contact_info
+      degaSpace {
+        id
+        name
+        slug
+        site_title
+        tag_line
+        description
+        site_address
+        logo {
+          url
         }
+        logo_mobile {
+          url
+        }
+        fav_icon {
+          url
+        }
+        mobile_icon {
+          url
+        }
+        verification_codes
+        social_media_urls
+        contact_info
       }
     }
   `);
   const formats = await graphql(`
     query FormatsQuery {
-      dega {
-        formats {
-          nodes {
-            id
-            slug
-          }
+      allDegaFormat {
+        nodes {
+          id
+          slug
         }
       }
     }
@@ -133,12 +126,10 @@ exports.createPages = async ({ graphql, actions, store, reporter }, { spaceId, h
 
   const tags = await graphql(`
     query TagsQuery {
-      dega {
-        tags {
-          nodes {
-            id
-            slug
-          }
+      allDegaTag {
+        nodes {
+          id
+          slug
         }
       }
     }
@@ -146,89 +137,30 @@ exports.createPages = async ({ graphql, actions, store, reporter }, { spaceId, h
 
   const categories = await graphql(`
     query CategoriesQuery {
-      dega {
-        categories(limit: 100, page: 1) {
-          nodes {
-            id
-            slug
-          }
+      allDegaCategory {
+        nodes {
+          id
+          slug
         }
       }
     }
   `);
   const users = await graphql(`
     query UsersQuery {
-      dega {
-        users {
-          nodes {
-            id
-          }
+      allDegaUser {
+        nodes {
+          id
         }
       }
     }
   `);
   const posts = await graphql(`
     query PostsQuery {
-      dega {
-        posts(limit: 100, page: 1) {
-          nodes {
-            id
-            published_date
-            slug
-          }
-        }
-      }
-    }
-  `);
-
-  const result = await graphql(`
-    query NodeQuery {
-      dega {
-        space {
+      allDegaPost {
+        nodes {
           id
-          name
+          published_date
           slug
-          site_title
-          tag_line
-          description
-          site_address
-          logo {
-            url
-          }
-          logo_mobile {
-            url
-          }
-          fav_icon {
-            url
-          }
-          mobile_icon {
-            url
-          }
-          verification_codes
-          social_media_urls
-          contact_info
-        }
-        sitemap {
-          tags {
-            id
-            slug
-          }
-          categories {
-            id
-            slug
-          }
-          users {
-            id
-            slug
-          }
-          posts {
-            id
-            slug
-          }
-          formats {
-            id
-            slug
-          }
         }
       }
     }
@@ -237,37 +169,27 @@ exports.createPages = async ({ graphql, actions, store, reporter }, { spaceId, h
   const format_factcheck = [];
   const format_without_factcheck = [];
 
-  formats.data.dega.formats.nodes
+  formats.data.allDegaFormat.nodes
     .filter((item) => item.slug === 'fact-check')
     .forEach((item) => {
-      format_factcheck.push(parseInt(item.id));
+      format_factcheck.push(item.id);
     });
-  // result.data.dega.sitemap.formats
-  //   .filter((item) => item.slug === 'fact-check')
-  //   .forEach((item) => {
-  //     format_factcheck.push(parseInt(item.id));
-  //   });
 
-  formats.data.dega.formats.nodes
+  formats.data.allDegaFormat.nodes
     .filter((item) => item.slug !== 'fact-check')
     .forEach((item) => {
-      format_without_factcheck.push(parseInt(item.id));
+      format_without_factcheck.push(item.id);
     });
-  // result.data.dega.sitemap.formats
-  //   .filter((item) => item.slug !== 'fact-check')
-  //   .forEach((item) => {
-  //     format_without_factcheck.push(parseInt(item.id));
-  //   });
-  // manifest
 
-  const icon = result.data.dega.space.fav_icon.url.proxy;
+  // manifest
+  const icon = space.data.degaSpace.fav_icon.url.proxy;
 
   const state = store.getState();
   const plugin = state.flattenedPlugins.find((plugin) => plugin.name === 'gatsby-plugin-manifest');
 
-  const resolveManifestOptions = async (data) => {
+  const resolveManifestOptions = (data) => {
     if (icon) {
-      await saveIcon(icon);
+      saveIcon(icon);
     } else {
       reporter.log('no favicon found on your space!!');
     }
@@ -282,13 +204,11 @@ exports.createPages = async ({ graphql, actions, store, reporter }, { spaceId, h
     };
   };
   if (plugin) {
-    const manifestOptions = await resolveManifestOptions(result.data.dega.space);
+    const manifestOptions = await resolveManifestOptions(space.data.degaSpace);
     plugin.pluginOptions = { ...plugin.pluginOptions, ...manifestOptions };
-    console.log(plugin.pluginOptions);
   }
 
   // homepage
-
   createPage({
     path: '/',
     component: require.resolve('./src/templates/homepage.js'),
@@ -299,57 +219,33 @@ exports.createPages = async ({ graphql, actions, store, reporter }, { spaceId, h
     },
   });
 
-  // if (homepage === 2) {
-  //   console.log('creating homepage v2');
-  //   createPage({
-  //     path: '/',
-  //     component: require.resolve('./src/templates/homepageTwo.js'),
-  //     context: {
-  //       sid: client,
-  //       homepage,
-  //     },
-  //   });
-  // }
-
-  formats.data.dega.formats.nodes.forEach((format) => {
+  formats.data.allDegaFormat.nodes.forEach((format) => {
     createPage({
       path: `/format/${format.slug}`,
       component: require.resolve('./src/templates/format-details.js'),
       context: {
-        id: parseInt(format.id),
+        id: format.id,
       },
     });
   });
 
-  // result.data.dega.sitemap.formats.forEach((format) => {
-  //   createPage({
-  //     path: `/formats/${format.slug}`,
-  //     component: require.resolve('./src/templates/format-details.js'),
-  //     context: {
-  //       id: parseInt(format.id),
-  //       sid: client,
-  //     },
-  //   });
-  // });
-
   // create post details page
 
-  posts.data.dega.posts.nodes.forEach((post) => {
+  posts.data.allDegaPost.nodes.forEach((post) => {
     if (post.published_date) {
       createPage({
         path: `/${post.slug}`,
         component: require.resolve('./src/templates/post-details.js'),
         context: {
-          id: parseInt(post.id),
+          id: post.id,
         },
       });
     }
-
     createPage({
       path: `/${post.slug}/amp/`,
       component: require.resolve('./src/templates/post-details.amp.js'),
       context: {
-        id: parseInt(post.id),
+        id: post.id,
         sid: spaceId,
       },
     });
@@ -357,174 +253,74 @@ exports.createPages = async ({ graphql, actions, store, reporter }, { spaceId, h
 
   // create tag details page
 
-  tags.data.dega.tags.nodes.forEach((tag) => {
+  tags.data.allDegaTag.nodes.forEach((tag) => {
     createPage({
       path: `/tag/${tag.slug}`,
       component: require.resolve('./src/templates/tag-details.js'),
       context: {
-        id: parseInt(tag.id),
+        id: tag.id,
       },
     });
 
     // create tag details page with each format
 
-    formats.data.dega.formats.nodes.forEach((format) => {
+    formats.data.allDegaFormat.nodes.forEach((format) => {
       createPage({
         path: `/tag/${tag.slug}/format/${format.slug}`,
         component: require.resolve('./src/templates/tag-details-format-details.js'),
         context: {
-          id: parseInt(tag.id),
-          format_id: parseInt(format.id),
+          id: tag.id,
+          format_id: format.id,
         },
       });
     });
   });
 
-  // result.data.dega.sitemap.tags.forEach((tag) => {
-  //   createPage({
-  //     path: `/tags/${tag.slug}`,
-  //     component: require.resolve('./src/templates/tag-details.js'),
-  //     context: {
-  //       id: parseInt(tag.id),
-  //       sid: client,
-  //     },
-  //   });
-
-  //   // create tag details page with each format
-
-  //   tags.data.dega.tags
-  //   result.data.dega.sitemap.formats.forEach((format) => {
-  //     createPage({
-  //       path: `/tags/${tag.slug}/formats/${format.slug}`,
-  //       component: require.resolve('./src/templates/tag-details-format-details.js'),
-  //       context: {
-  //         id: parseInt(tag.id),
-  //         format_id: parseInt(format.id),
-  //         sid: client,
-  //       },
-  //     });
-  //   });
-  // });
-
   // create category details page
 
-  categories.data.dega.categories.nodes.forEach((category) => {
+  categories.data.allDegaCategory.nodes.forEach((category) => {
     createPage({
       path: `/category/${category.slug}`,
       component: require.resolve('./src/templates/category-details.js'),
       context: {
-        id: parseInt(category.id),
+        id: category.id,
       },
     });
 
     // create category details page with each format
-    formats.data.dega.formats.nodes.forEach((format) => {
+    formats.data.allDegaFormat.nodes.forEach((format) => {
       createPage({
         path: `/category/${category.slug}/format/${format.slug}`,
         component: require.resolve('./src/templates/category-details-format-details.js'),
         context: {
-          id: parseInt(category.id),
-          format_id: parseInt(format.id),
+          id: category.id,
+          format_id: format.id,
         },
       });
     });
   });
 
-  // result.data.dega.sitemap.categories.forEach((category) => {
-  //   createPage({
-  //     path: `/categories/${category.slug}`,
-  //     component: require.resolve('./src/templates/category-details.js'),
-  //     context: {
-  //       id: parseInt(category.id),
-  //       sid: client,
-  //     },
-  //   });
-
-  //   // create category details page with each format
-
-  //   result.data.dega.sitemap.formats.forEach((format) => {
-  //     createPage({
-  //       path: `/categories/${category.slug}/formats/${format.slug}`,
-  //       component: require.resolve('./src/templates/category-details-format-details.js'),
-  //       context: {
-  //         id: parseInt(category.id),
-  //         format_id: parseInt(format.id),
-  //         sid: client,
-  //       },
-  //     });
-  //   });
-  // });
-
   // create user details page
-  users.data.dega.users.nodes.forEach((user) => {
+  users.data.allDegaUser.nodes.forEach((user) => {
     createPage({
       path: `/author/${user.id}`,
       component: require.resolve('./src/templates/author-details.js'),
       context: {
-        id: parseInt(user.id),
+        id: user.id,
       },
     });
 
     // create user details page with each format
 
-    formats.data.dega.formats.nodes.forEach((format) => {
+    formats.data.allDegaFormat.nodes.forEach((format) => {
       createPage({
         path: `/author/${user.id}/format/${format.slug}`,
         component: require.resolve('./src/templates/author-details-format-details.js'),
         context: {
-          id: parseInt(user.id),
-          format_id: parseInt(format.id),
+          id: user.id,
+          format_id: format.id,
         },
       });
     });
-
-    // result.data.dega.sitemap.formats.forEach((format) => {
-    //   createPage({
-    //     path: `/users/${user.id}/formats/${format.slug}`,
-    //     component: require.resolve('./src/templates/user-details-format-details.js'),
-    //     context: {
-    //       id: parseInt(user.id),
-    //       format_id: parseInt(format.id),
-    //       sid: client,
-    //     },
-    //   });
-    // });
   });
-
-  // result.data.dega.sitemap.users.forEach((user) => {
-  //   createPage({
-  //     path: `/users/${user.id}`,
-  //     component: require.resolve('./src/templates/user-details.js'),
-  //     context: {
-  //       id: parseInt(user.id),
-  //       sid: client,
-  //     },
-  //   });
-
-  //   // create user details page with each format
-
-  //   formats.data.dega.formats.nodes.forEach((format) => {
-  //     createPage({
-  //       path: `/users/${user.id}/formats/${format.slug}`,
-  //       component: require.resolve('./src/templates/user-details-format-details.js'),
-  //       context: {
-  //         id: parseInt(user.id),
-  //         format_id: parseInt(format.id),
-  //         sid: client,
-  //       },
-  //     });
-  //   });
-
-  //   // result.data.dega.sitemap.formats.forEach((format) => {
-  //   //   createPage({
-  //   //     path: `/users/${user.id}/formats/${format.slug}`,
-  //   //     component: require.resolve('./src/templates/user-details-format-details.js'),
-  //   //     context: {
-  //   //       id: parseInt(user.id),
-  //   //       format_id: parseInt(format.id),
-  //   //       sid: client,
-  //   //     },
-  //   //   });
-  //   // });
-  // });
 };
