f5eac5e739b81add64a46979b4be15e1b170c5f0
Chakradhar B <chakradhar2711@gmail.com>
2022-02-17 23:53:17 +0530

    revert back ssr config for youtube site

--- examples/factly-youtube/gatsby-ssr.js
+++ examples/factly-youtube/gatsby-ssr.js
@@ -8,11 +8,6 @@ export const onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
     defer
     data-domain="videos.factly.in"
     src="https://plausible.factly.in/js/index.js"
-  ></script>,
-  <script key={2} async src="https://www.googletagmanager.com/gtag/js?id=UA-181770922-1"></script>, 
-    <script key={3}> 
-      window.dataLayer = window.dataLayer || []; 
-      function gtag(){dataLayer.push(arguments)} gtag('js', new Date()); gtag('config', 'UA-181770922-1'); 
-    </script>
+  ></script>
   ])
 }
