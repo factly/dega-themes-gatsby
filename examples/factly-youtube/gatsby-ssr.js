4ac2ebb41d6d3b687cf3de4c9cccf85a6fbb72c5
Chakradhar B <chakradhar2711@gmail.com>
2022-02-17 22:57:15 +0530

    add google analytics tag for factly youtube site

--- examples/factly-youtube/gatsby-ssr.js
+++ examples/factly-youtube/gatsby-ssr.js
@@ -3,11 +3,16 @@ import React from 'react';
 export const onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
   setPostBodyComponents([
     <script
-      key={1}
-      async
-      defer
-      data-domain="videos.factly.in"
-      src="https://plausible.factly.in/js/index.js"
-    ></script>,
-  ]);
-};
+    key={1}
+    async
+    defer
+    data-domain="videos.factly.in"
+    src="https://plausible.factly.in/js/index.js"
+  ></script>,
+  <script key={2} async src="https://www.googletagmanager.com/gtag/js?id=UA-181770922-1"></script>, 
+    <script key={3}> 
+      window.dataLayer = window.dataLayer || []; 
+      function gtag(){dataLayer.push(arguments)} gtag('js', new Date()); gtag('config', 'UA-181770922-1'); 
+    </script>
+  ])
+}
