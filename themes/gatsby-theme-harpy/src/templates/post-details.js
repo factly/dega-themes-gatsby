80d6aa53dba36d794c616d9856af0a6ab13c6d0c
Chakradhar B <chakradhar2711@gmail.com>
2022-04-20 15:11:14 +0530

    add recent posts and remove infinite scrolling for the themes

--- themes/gatsby-theme-harpy/src/templates/post-details.js
+++ themes/gatsby-theme-harpy/src/templates/post-details.js
@@ -1,47 +1,36 @@
 /** @jsx jsx */
 import React from 'react';
 import { graphql } from 'gatsby';
-import InfiniteScroll from 'react-infinite-scroller';
-
 import { jsx } from 'theme-ui';
 import Post from '@components/Post/index.js';
 import Layout from '@components/Layout/index';
 import { isBrowser } from '@utils/isBrowser';
 import Seo from '@components/Seo';
-import { FaTwitterSquare, FaFacebookSquare, FaWhatsappSquare } from 'react-icons/fa';
+import {
+  FaTwitterSquare,
+  FaFacebookSquare,
+  FaWhatsappSquare,
+  FaChevronLeft,
+  FaChevronRight,
+} from 'react-icons/fa';
+import { Link } from 'gatsby';
+import parseDate from '@utils/parseDate';
 
 /**
  * TODO: Add loader for infinite-scroller
  */
 const PostDetails = ({ data }) => {
-  const { allDegaPost, degaSpace, degaPost } = data;
-  const filteredPosts = allDegaPost.nodes.filter((post) => post.published_date !== null);
-  const posts = filteredPosts.filter((post) => post.id !== degaPost.id);
-  posts.unshift(degaPost);
+  const { allDegaPost, degaSpace, degaPost, recentPosts } = data;
+  const { edges: posts } = allDegaPost;
+  const post = posts.filter(({ node }) => node.id === degaPost.id)[0];
+  const { previous: previousPost, next: nextPost } = post;
+  console.log({ previousPost, nextPost });
 
-  const [postItems, setPostItems] = React.useState(posts.slice(0, 1));
-  const [hasNextPage, setHasNextPage] = React.useState(true);
   const [showSocialIcon, setShowSocialIcon] = React.useState(false);
-  const [postActiveIndex, setPostActiveIndex] = React.useState(parseInt(degaPost.id));
-  const [relatedPosts, setRelatedPosts] = React.useState(posts.slice(0, 10));
-  const [hasNextPageRelatedPost, setHasNextPageRelatedPost] = React.useState(true);
+
   const [observer, setObserver] = React.useState({
-    observe: () => { },
+    observe: () => {},
   });
-  const handleLoadMore = () => {
-    if (!hasNextPage) return false;
-    const nextPageItems = posts.slice(postItems.length, postItems.length + 1);
-    setPostItems([...postItems, ...nextPageItems]);
-    setHasNextPage(postItems.length < posts.length);
-  };
-
-  const handleLoadMoreRelatedPosts = () => {
-    if (!hasNextPageRelatedPost) return false;
-
-    const nextPageItems = posts.slice(relatedPosts.length, relatedPosts.length + 10);
-    setRelatedPosts([...relatedPosts, ...nextPageItems]);
-    setHasNextPageRelatedPost(relatedPosts.length < posts.length);
-  };
 
   const handleShowSocialIcon = (entry) => {
     if (entry.intersectionRatio > 0) {
@@ -51,24 +40,11 @@ const PostDetails = ({ data }) => {
     }
   };
 
-  const handleSetActiveLink = (entry) => {
-    const id = entry.target.getAttribute('slug');
-    if (entry.intersectionRatio > 0) {
-      setPostActiveIndex(id);
-      if (isBrowser) {
-        window.history.pushState('page2', 'Title', `/${id}`);
-      }
-    }
-  };
-
   const createObserver = () => {
     const o = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.target.hasAttribute('social-icon')) {
           handleShowSocialIcon(entry);
-        }
-        if (entry.target.hasAttribute('post')) {
-          handleSetActiveLink(entry);
         }
       });
     });
@@ -118,99 +94,117 @@ const PostDetails = ({ data }) => {
             pl: (theme) => [null, null, `${theme.space.spacing8}`],
           }}
         >
-          <InfiniteScroll
-            pageStart={0}
-            loadMore={handleLoadMore}
-            hasMore={hasNextPage}
-            loader={
-              <div className="loader" key={0}>
-                Loading...
+          <Post key={`details${degaPost.id}`} post={degaPost} observer={observer} />
+          <div>
+            <div
+              sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                mt: (theme) => `${theme.space.spacing6}`,
+                pb: (theme) => `${theme.space.spacing6}`,
+                borderBottomWidth: '1px',
+              }}
+            >
+              <div sx={{ flex: '0 0 50%', maxWidth: '50%', p: '1.5rem', textAlign: 'left' }}>
+                {previousPost && (
+                  <>
+                    <Link
+                      to={`/${previousPost.slug}`}
+                      sx={{ display: 'flex', alignItems: 'center', gap: '1rem' }}
+                    >
+                      <span>
+                        <FaChevronLeft />
+                      </span>
+                      <div>
+                        <span sx={{ fontSize: '0.75rem', textTransform: 'uppercase' }}>
+                          Previous Post
+                        </span>
+                        <h3>{previousPost.title}</h3>
+                      </div>
+                    </Link>
+                  </>
+                )}
               </div>
-            }
-          >
-            {postItems.map((item) => (
-              <Post key={`details${item.id}`} post={item} observer={observer} />
-            ))}
-          </InfiniteScroll>
-          {showSocialIcon && (
-            <>
-              <div
-                className="top-auto"
-                style={{
-                  top: '40vh',
-                }}
+              <div sx={{ flex: '0 0 50%', maxWidth: '50%', p: '1.5rem', textAlign: 'right' }}>
+                {nextPost && (
+                  <>
+                    <Link
+                      to={`/${nextPost.slug}`}
+                      sx={{ display: 'flex', alignItems: 'center', gap: '1rem' }}
+                    >
+                      <div>
+                        <span sx={{ fontSize: '0.75rem', textTransform: 'uppercase' }}>
+                          Next Post
+                        </span>
+                        <h3>{nextPost.title}</h3>
+                      </div>
+                      <span>
+                        <FaChevronRight />
+                      </span>
+                    </Link>
+                  </>
+                )}
+              </div>
+            </div>
+            <div
+              sx={{
+                mt: (theme) => `${theme.space.spacing6}`,
+                pb: (theme) => `${theme.space.spacing6}`,
+                borderBottomWidth: '1px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+              }}
+            >
+              <h5
                 sx={{
-                  display: ['none', null, 'flex'],
-                  flexDirection: 'column',
-                  position: 'fixed',
-                  ml: (theme) => `-${theme.space.spacing8}`,
-                  // left: 0,
-                  alignItems: 'center',
-                  justifyContent: ['flex-start', null, 'flex-end'],
-                  top: '40vh',
+                  textAlign: 'center',
+                  position: 'relative',
+                  alignSelf: 'center',
+                  mb: '1.5rem',
+                  '&:after': {
+                    position: 'absolute',
+                    content: '""',
+                    width: '50%',
+                    height: '1px',
+                    borderBottom: '2px solid #3BB2F6',
+                    bottom: '-2px',
+                    left: '50%',
+                    marginLeft: '-25%',
+                  },
                 }}
               >
-                <a
-                  title="Share on Facebook"
-                  href={`https://www.facebook.com/sharer.php?u=${url}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  sx={{
-                    display: 'block',
-                    m: (theme) => `${theme.space.spacing1}`,
-                    p: (theme) => `${theme.space.spacing3}`,
-                    '&:first-of-type': { mx: 0 },
-                    fontWeight: 'semibold',
-                    borderRadius: 'default',
-                  }}
-                >
-                  <FaFacebookSquare
-                    sx={{ fontSize: (theme) => `${theme.fontSizes.h4}` }}
-                    color="#3b5998"
-                  />
-                </a>
-                <a
-                  title="Tweet it"
-                  href={`https://twitter.com/share?text=${title}-&url=${url}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  sx={{
-                    display: 'block',
-                    m: (theme) => `${theme.space.spacing1}`,
-                    '&:first-of-type': { mx: 0 },
-                    p: (theme) => `${theme.space.spacing3}`,
-                    fontWeight: 'semibold',
-                    borderRadius: 'default',
-                  }}
-                >
-                  <FaTwitterSquare
-                    sx={{ fontSize: (theme) => `${theme.fontSizes.h4}` }}
-                    color="#1da1f2"
-                  />
-                </a>
-                <a
-                  title="Share on WhatsApp"
-                  href={`https://api.whatsapp.com/send?text=${encodeURIComponent(`${title} - ${url}`)}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  sx={{
-                    display: 'block',
-                    m: (theme) => `${theme.space.spacing1}`,
-                    '&:first-of-type': { mx: 0 },
-                    p: (theme) => `${theme.space.spacing3}`,
-                    fontWeight: 'semibold',
-                    borderRadius: 'default',
-                  }}
-                >
-                  <FaWhatsappSquare
-                    sx={{ fontSize: (theme) => `${theme.fontSizes.h4}` }}
-                    color="#25d366"
-                  />
-                </a>
+                Recent Posts
+              </h5>
+              <div sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                {recentPosts.nodes
+                  .filter((post) => post.id !== degaPost.id)
+                  .splice(0, 6)
+                  .map((post) => (
+                    <div
+                      key={post.id}
+                      sx={{
+                        flex: '0 0 50%',
+                        maxWidth: '50%',
+                        p: '1.5rem',
+                        textAlign: 'left',
+                      }}
+                    >
+                      <Link to={`/${post.slug}`} sx={{ display: 'flex' }}>
+                        <div sx={{ flex: '0 0 33%' }}>
+                          <img src={post.medium.url.proxy} alt="" />
+                        </div>
+                        <div sx={{ flex: '0 0 67%', pl: '1rem' }}>
+                          {' '}
+                          <h5>{post.title}</h5>{' '}
+                          <p sx={{ fontSize: '0.75rem' }}>{parseDate(post.published_date)}</p>{' '}
+                        </div>
+                      </Link>
+                    </div>
+                  ))}
               </div>
-
-            </>
-          )}
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
@@ -224,65 +218,79 @@ export const query = graphql`
       site_address
     }
     allDegaPost {
-      nodes {
-        published_date
-        description
-        excerpt
-        id
-        schemas
-        slug
-        status
-        subtitle
-        title
-        updated_at
-        users {
-          email
-          first_name
-          last_name
-          display_name
-          id
-          slug
-        }
-        tags {
-          id
-          name
-          slug
+      edges {
+        node {
+          published_date
           description
-        }
-        medium {
-          alt_text
+          excerpt
           id
-          url
-          dimensions
-        }
-        format {
-          name
+          schemas
           slug
-          id
-          description
-        }
-        claims {
-          checked_date
-          claim_date
-          claim_sources
-          claimant {
-            description
+          status
+          subtitle
+          title
+          updated_at
+          users {
+            email
+            first_name
+            last_name
+            id
+            slug
+          }
+          tags {
             id
             name
             slug
-            tag_line
+            description
           }
-          description
-          id
-          fact
-          review_sources
-          slug
-          claim
-          rating {
+          medium {
+            alt_text
+            id
+            url
+            dimensions
+          }
+          format {
+            name
+            slug
+            id
+            description
+          }
+          claims {
+            checked_date
+            claim_date
+            claim_sources
+            claimant {
+              description
+              id
+              name
+              slug
+              tag_line
+            }
             description
             id
+            fact
+            review_sources
+            slug
+            claim
+            rating {
+              description
+              id
+              name
+              numeric_value
+              slug
+              medium {
+                alt_text
+                id
+                url
+                dimensions
+              }
+            }
+          }
+          categories {
+            description
+            created_at
+            id
             name
-            numeric_value
             slug
             medium {
               alt_text
@@ -292,12 +300,21 @@ export const query = graphql`
             }
           }
         }
-        categories {
-          description
-          created_at
-          id
-          name
+        next {
           slug
+          title
+          published_date
+          medium {
+            alt_text
+            id
+            url
+            dimensions
+          }
+        }
+        previous {
+          slug
+          title
+          published_date
           medium {
             alt_text
             id
@@ -385,6 +402,60 @@ export const query = graphql`
           id
           url
           dimensions
+        }
+      }
+    }
+    recentPosts: allDegaPost(
+      sort: { fields: created_at, order: DESC }
+      filter: { format: { slug: { eq: "article" } } }
+      limit: 6
+    ) {
+      nodes {
+        created_at
+        title
+        excerpt
+        slug
+        users {
+          display_name
+          slug
+          id
+        }
+        published_date
+        categories {
+          name
+          slug
+        }
+        medium {
+          dimensions
+          alt_text
+          url
+        }
+      }
+    }
+    recentFactChecks: allDegaPost(
+      sort: { fields: created_at, order: DESC }
+      filter: { format: { slug: { eq: "fact-check" } } }
+      limit: 6
+    ) {
+      nodes {
+        created_at
+        title
+        excerpt
+        slug
+        users {
+          display_name
+          slug
+          id
+        }
+        published_date
+        categories {
+          name
+          slug
+        }
+        medium {
+          dimensions
+          alt_text
+          url
         }
       }
     }
