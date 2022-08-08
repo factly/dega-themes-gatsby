7455d273b40af1f37dd5f3add73c3b69687adb17
Chakradhar B <chakradhar2711@gmail.com>
2022-08-08 11:40:43 +0530

    add quail-theme

---
+++ themes/gatsby-theme-quail/src/components/Post/Badge.js
@@ -0,0 +1,49 @@
+/** @jsx jsx */
+import React from 'react'; // eslint-disable-line no-unused-vars
+import { Link } from 'gatsby';
+import { jsx } from 'theme-ui';
+
+/**
+ * TODO: 1. Add transitions, borderWidth, borderRadius to theme-ui
+ */
+const Badge = ({ url, name }) => {
+  return (
+    <Link
+      to={`/category/${url}`}
+      // sx={{
+      //   m: (theme) => `${theme.space.spacing3}`,
+      //   bg: '#000',
+      //   transition: '0.3s',
+      //   '&:hover': {
+      //     //color: 'white',
+      //     bg: 'green',
+      //   },
+      //   display: 'inline-flex',
+      //   px: '20px',
+      //   height: '32px',
+      //   lineHeight: '32px',
+      //   fontSize: '.875rem',
+      //   fontWeight: 500,
+      //   position: 'relative',
+      //   borderRadius: '16px',
+      //   color: 'white',
+      // }}
+      sx={{
+        fontWeight: 500,
+        fontSize: '14px',
+        lineHeight: '20px',
+        textAlign: 'center',
+        color: '#7855F7',
+        background: '#F4F1FF',
+        borderRadius: '16px',
+        alignItems: 'center',
+        padding: '2px 10px',
+
+      }}
+    >
+      {name}
+    </Link>
+  );
+};
+
+export default Badge;
---
+++ themes/gatsby-theme-quail/utils/youtubeSourceSchema.js
@@ -0,0 +1,162 @@
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+  const typeDefs = `
+  type Channel implements Node {
+    id: ID!
+    image: File @link(from: "image___NODE")
+    kind: String
+    etag: String
+    snippet: ChannelSnippet
+    contentDetails: ChannelContentDetails
+    statistics: ChannelStatistics
+    channelId: String
+    parent: Node
+    children: [Node!]!
+    internal: Internal!
+  }
+  type Playlist implements Node {
+    id: ID!
+    image: File @link(from: "image___NODE")
+    videos: [Video] @link
+    kind: String
+    etag: String
+    snippet: PlaylistSnippet
+    contentDetails: ContentDetails
+    playlistId: String
+    list: Boolean
+    parent: Node
+    children: [Node!]!
+    internal: Internal!
+  }
+  type PlaylistSnippet {
+    publishedAt: Date @dateformat
+    channelId: String
+    title: String
+    description: String
+    thumbnails: SnippetThumbnails
+    channelTitle: String
+    localized: Localized
+  }
+  type ChannelSectionsPlaylist {
+    id: String
+    list: Boolean
+    snippet: ChannelSectionsPlaylistSnippet
+    contentDetails: ContentDetails
+    videos: [String]
+    playlistId: String
+    kind: String
+    etag: String
+  }
+  type ChannelSectionsPlaylistSnippet {
+    title: String
+    channelTitle: String
+    publishedAt: Date @dateformat
+    channelId: String
+    description: String
+    thumbnails: Thumbnails
+    localized: Localized
+  }
+  type ContentDetails {
+    itemCount: Int
+  }
+  type Video implements Node {
+    id: ID!
+    image: File @link(from: "image___NODE")
+    playlistIds: [String]
+    positions: [VideoPositions]
+    kind: String
+    etag: String
+    snippet: VideoSnippet
+    contentDetails: VideoContentDetails
+    parent: Node
+    children: [Node!]!
+    internal: Internal!
+  }
+
+  type ChannelSections implements Node {
+    id: ID!
+    videos: [Video] @link
+    kind: String
+    etag: String
+    snippet: ChannelSectionsSnippet
+    playlist: ChannelSectionsPlaylist
+    contentDetails: ContentDetails
+    parent: Node
+    children: [Node!]!
+    internal: Internal!
+  }
+  type ChannelSectionsSnippet {
+    type: String
+    style: String
+    channelId: String
+    position: Int
+  }
+  type ChannelSnippet {
+    title: String
+    description: String
+    customUrl: String
+    publishedAt: Date @dateformat
+    thumbnails: ChannelSnippetThumbnails
+    localized: Localized
+    country: String
+  }
+  type ChannelSnippetThumbnails {
+    default: Thumbnails
+    medium: Thumbnails
+    high: Thumbnails
+  }
+  type Localized {
+    title: String
+    description: String
+  }
+  type ChannelContentDetails {
+    relatedPlaylists: ChannelContentDetailsRelatedPlaylists
+  }
+  type ChannelContentDetailsRelatedPlaylists {
+    likes: String
+    favorites: String
+    uploads: String
+  }
+  type ChannelStatistics {
+    viewCount: String
+    subscriberCount: String
+    hiddenSubscriberCount: Boolean
+    videoCount: String
+  }
+  type VideoPositions {
+    position: Int
+    playlist: String
+  }
+  type VideoSnippet {
+    publishedAt: Date @dateformat
+    channelId: String
+    title: String
+    description: String
+    thumbnails: SnippetThumbnails
+    channelTitle: String
+    playlistId: String
+    position: Int
+    resourceId: VideoSnippetResourceId
+  }
+  type VideoContentDetails {
+    videoId: String
+    videoPublishedAt: Date @dateformat
+  }
+  type SnippetThumbnails {
+    default: Thumbnails
+    medium: Thumbnails
+    high: Thumbnails
+    standard: Thumbnails
+    maxres: Thumbnails
+  }
+  type Thumbnails {
+    url: String
+    width: Int
+    height: Int
+  }
+  type VideoSnippetResourceId {
+    kind: String
+    videoId: String
+  }`;
+  createTypes(typeDefs);
+};
